989822b9367eb2132720600dbfd4107bc3fd50ca
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2021-03-16 09:28:14 +0100

    remove Distribtuions from make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,7 @@
 using Documenter
-using MissingStrategies, Distributions, StatsBase, StatsPlots
+using MissingStrategies
 
 push!(LOAD_PATH,"../src/")
-# need to add Statistics and Distributions to Project.toml in docs/
-#DocMeta.setdocmeta!(MissingStrategies, :DocTestSetup, :(using Statistics,Distributions,MissingStrategies); recursive=true)
-#DocMeta.setdocmeta!(Distributions, :DocTestSetup, :(using Statistics,Distributions,MissingStrategies); recursive=true)
 makedocs(sitename="MissingStrategies.jl",
          doctest  = false, 
          pages = [
